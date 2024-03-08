85cc842371fcda7eda4249afbd70a8d9d82e5e8e
Jhenyffer <140458083+JhenyfferOliveira@users.noreply.github.com>
2024-03-08 00:03:19 -0300

    Add files via upload

---
+++ cursos/forms.py
@@ -0,0 +1,15 @@
+﻿from django import forms
+from cursos.models import Curso
+from datetime import date
+
+class CursoForm(forms.ModelForm): # classe, campos (field), widget
+    def clean_data_do_curso(self):
+        data_do_curso = self.cleaned_data['data_do_curso']
+        hoje = date.today()
+        if data_do_curso < hoje:
+             raise forms.ValidationError('Não é possível cadastrar um curso no passado!')
+        return data_do_curso
+
+    class Meta: 
+        model = Curso
+        fields = ['titulo', 'nivel', 'carga_horaria', 'data_do_curso', 'descricao']
---
+++ cursos/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from cursos.forms import CursoForm
+from django.views.decorators.cache import cache_page
+from cursos.models import Curso
+
+# Create your views here.
+def inicio_curso(request):
+    cursos = Curso.objects.all()
+    return render(request, 'inicio_curso.html', {'cursos': cursos})
+
+@cache_page(30)
+def criar_curso(request):
+    cursos = Curso.objects.all()
+    form = CursoForm(request.POST or None)
+    sucesso = False
+    if form.is_valid():
+        form.save()
+        sucesso = True
+    contexto = {
+        'form': form,
+        'sucesso': sucesso,
+        'cursos': cursos,
+    }
+    return render(request, 'criar_curso.html', contexto)
