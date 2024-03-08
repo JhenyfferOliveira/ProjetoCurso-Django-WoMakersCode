9ce669ae9d639065246c415b02530fb5b4bdfc2f
Jhenyffer <140458083+JhenyfferOliveira@users.noreply.github.com>
2024-03-07 22:14:09 -0300

    Add files via upload

---
+++ cursos/tests/test_views.py
@@ -0,0 +1,8 @@
+﻿from pytest_django.asserts import assertTemplateNotUsed
+import pytest
+
+@pytest.mark.django_db
+def test_curso_view_deve_retornar_template(client):
+    response = client.get('curso/criar_curso')
+    assert response.status_code== 200
+    assertTemplateNotUsed(response, 'criar_curso.html')
