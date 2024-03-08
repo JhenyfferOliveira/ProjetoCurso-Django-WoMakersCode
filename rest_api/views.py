9da097087313717c15ba338fc9ebedb99bdbbba8
Jhenyffer <140458083+JhenyfferOliveira@users.noreply.github.com>
2024-03-07 22:18:58 -0300

    Add files via upload

---
+++ rest_api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from cursos.models import Curso
+from rest_api.serializers import CursoModelSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({'Message': f'Hello, {request.data.get('name')}'})
+    return Response({'Hello': 'Hello World API'})
+
+class CursoModelViewSet(ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoModelSerializer
